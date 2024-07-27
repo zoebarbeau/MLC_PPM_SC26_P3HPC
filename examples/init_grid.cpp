1bacab5057434987573c019c03bb7a6bc8a92c02
zoebarbeau <70536046+zoebarbeau@users.noreply.github.com>
2024-07-26 17:09:57 -0700

    Add files via upload

---
+++ examples/init_grid.cpp
@@ -0,0 +1,188 @@
+#include <ExaMPM_BoundaryConditions.hpp>
+#include <ExaMPM_Solver2.hpp>
+
+#include <Cabana_Core.hpp>
+
+#include <Cabana_Grid.hpp>
+
+#include <Kokkos_Core.hpp>
+
+#include <mpi.h>
+
+#include <array>
+#include <cmath>
+
+//---------------------------------------------------------------------------//
+// Create the problem setup. The initial geometry is a static water column
+// from [0,0.4] in X, [0,0.6] in Z, with the entire Y domain filled.
+struct ParticleInitFunc
+{
+    double _vorticity;
+    double _h;
+    ParticleInitFunc( const double cell_size, const double amp )
+        : _vorticity( amp ), _h(cell_size)
+    {
+    }
+
+    template <class ParticleType>
+    KOKKOS_INLINE_FUNCTION bool operator()( const double x[3],
+                                            ParticleType& p ) const
+    {   
+	double r,s,theta;
+        double pi = 2*acos(0.0);
+        double magn,vortx, vorty, vortz;
+	r = pow( pow(x[0], 2.0) + pow(x[1],2.0),  0.5);
+	s = pow( pow(r-0.5, 2.0) + pow(x[2], 2.0), 0.5);
+
+        if ( (r <= 0.65) && (r >= 0.35) )
+        {  
+
+            if( (s < 0.15+_h) && (s > 0.15 - _h) ){		
+	    
+/*	      if( x[0] == 0 && x[1] > 0 ){
+                  theta = pi/2;
+
+	      }else if( x[0] == 0 && x[1] < 0){
+                  theta = -pi/2;
+
+	      }else{
+                 
+                 theta =atanh(x[1] / (x[0]));
+		 std::cout << "theta " << theta << std::endl;
+	      }	      
+*/	           
+              // Vorticity
+              vortz = 0;
+	      vortx = -r*x[1]; vorty = r*x[0];
+	      magn = pow( pow(vortx, 2.0) + pow(vorty, 2.0), 0.5);
+	      vortx = vortx/magn; vorty = vorty/magn;
+	      Cabana::get<0>( p, 0 ) = vortx;
+              Cabana::get<0>( p, 1 ) = vorty;
+	      Cabana::get<0>( p, 2) = 0.0;
+
+              // Velocity
+              for ( int d = 0; d < 3; ++d )
+                Cabana::get<1>( p, d ) = 0.0;
+
+              // Position
+              for ( int d = 0; d < 3; ++d )
+                 Cabana::get<2>( p, d ) = x[d];
+	     
+	      Cabana::get<3>(p) = 1.0;
+	      return true;
+	    } 
+        }
+
+        return false;
+    }
+};
+
+//---------------------------------------------------------------------------//
+void initgrid( const double cell_size, const int ppc, const int halo_size,
+               const std::string& exec_space, const double vorticity )
+{
+    // The dam break domain is in a box on [0,1] in each dimension.
+    Kokkos::Array<double, 6> global_box = { -0.75, -0.75, -0.75, 0.75, 0.75, 0.75 };
+
+    // Compute the number of cells in each direction. The user input must
+    // squarely divide the domain.
+    std::array<int, 3> global_num_cell = {
+        static_cast<int>( 1.5 / cell_size ),
+        static_cast<int>( 1.5 / cell_size ),
+        static_cast<int>( 1.5 / cell_size ) };
+
+    // This will look like a 2D problem so make the Y direction periodic.
+    std::array<bool, 3> periodic = { false, false, false };
+
+    // Due to the 2D nature of the problem we will only partition in Y. The
+    // behavior of the fluid will be to largely just run out in X and Z with
+    // little movement in Y.
+    int comm_size;
+    MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
+    std::array<int, 3> ranks_per_dim = { 1, comm_size, 1 };
+    Cabana::Grid::ManualBlockPartitioner<3> partitioner( ranks_per_dim );
+
+
+    // Free slip conditions (alternative: NO_SLIP)
+    ExaMPM::BoundaryCondition bc;
+    bc.boundary[0] = ExaMPM::BoundaryType::NO_SLIP;
+    bc.boundary[1] = ExaMPM::BoundaryType::NO_SLIP;
+    bc.boundary[2] = ExaMPM::BoundaryType::NO_SLIP;
+    bc.boundary[3] = ExaMPM::BoundaryType::NO_SLIP;
+    bc.boundary[4] = ExaMPM::BoundaryType::NO_SLIP;
+    bc.boundary[5] = ExaMPM::BoundaryType::NO_SLIP;
+    double t_final = 1.0;
+    int write_freq = 1;
+    // Solve the problem.
+    auto solver = ExaMPM::createSolver(
+        exec_space, MPI_COMM_WORLD, global_box, global_num_cell, periodic,
+        partitioner, halo_size, ParticleInitFunc( cell_size,vorticity ),vorticity, ppc, bc );
+    solver->solve( t_final, write_freq );
+}
+
+//---------------------------------------------------------------------------//
+int main( int argc, char* argv[] )
+{
+    MPI_Init( &argc, &argv );
+
+    Kokkos::initialize( argc, argv );
+
+    // check inputs and write usage
+    if ( argc < 5 )
+    {
+        std::cerr << "Usage: ./init_grid cell_size parts_per_cell_size "
+                     "halo_cells exec_space vorticity\n";
+        std::cerr << "\nwhere cell_size       edge length of a computational "
+                     "cell (domain is unit cube)\n";
+        std::cerr
+            << "      parts_per_cell  particles per cell in each direction\n";
+        std::cerr << "      halo_cells      number of halo cells\n";
+        std::cerr << "      dt              time step size\n";
+        std::cerr << "      t_end           simulation end time\n";
+        std::cerr
+            << "      write_freq      number of steps between output files\n";
+        std::cerr << "      exec_space      execute with: serial, openmp, "
+                     "cuda, hip\n";
+        std::cerr << "\nfor example: ./init_grid 0.05 2 0 serial 1\n";
+        Kokkos::finalize();
+        MPI_Finalize();
+        return 0;
+    }
+
+    // cell size
+    double cell_size = std::atof( argv[1] );
+
+    // particles per cell in a dimension
+    int ppc = std::atoi( argv[2] );
+
+    // number of halo cells.
+    int halo_size = std::atoi( argv[3] );
+
+    // time step size.
+//    double delta_t = std::atof( argv[4] );
+
+    // end time.
+//    double t_final = std::atof( argv[5] );
+
+    // write frequency
+//    int write_freq = std::atoi( argv[6] );
+
+    // execution space
+    std::string exec_space( argv[4] );
+
+    //vorticity
+    //
+    double vorticity = std::atof( argv[5] );
+
+    // run the problem.
+    initgrid( cell_size, ppc, halo_size,
+              exec_space, vorticity );
+
+    Kokkos::finalize();
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+//---------------------------------------------------------------------------//
