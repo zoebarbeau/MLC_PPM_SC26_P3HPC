e99e4563f9378ad6bc81fbf59e010e45e5a3b96d
zoebarbeau <70536046+zoebarbeau@users.noreply.github.com>
2024-07-26 17:12:31 -0700

    Add files via upload

---
+++ src/ExaMPM_Solver2.hpp
@@ -0,0 +1,196 @@
+/****************************************************************************
+ * Copyright (c) 2018-2020 by the ExaMPM authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ExaMPM library. ExaMPM is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef EXAMPM_SOLVER2_HPP
+#define EXAMPM_SOLVER2_HPP
+
+#include <ExaMPM_BoundaryConditions.hpp>
+#include <ExaMPM_Mesh.hpp>
+#include <ExaMPM_ProblemManager2.hpp>
+#include <ExaMPM_DriverGrid.hpp>
+#include <Cabana_Core.hpp>
+#include <Kokkos_Core.hpp>
+
+#include <memory>
+#include <string>
+
+#include <mpi.h>
+
+namespace ExaMPM
+{
+//---------------------------------------------------------------------------//
+class SolverBase
+{
+  public:
+    virtual ~SolverBase() = default;
+    virtual void solve( const double t_final, const int write_freq ) = 0;
+};
+
+//---------------------------------------------------------------------------//
+template <class MemorySpace, class ExecutionSpace>
+class Solver : public SolverBase
+{
+  public:
+    template <class InitFunc>
+    Solver( MPI_Comm comm, const Kokkos::Array<double, 6>& global_bounding_box,
+            const std::array<int, 3>& global_num_cell,
+            const std::array<bool, 3>& periodic,
+            const Cabana::Grid::BlockPartitioner<3>& partitioner,
+            const int halo_cell_width, const InitFunc& create_functor,
+            const int particles_per_cell, const double vorticity,
+            const BoundaryCondition& bc )
+        : _dt( 0.001 )
+        , _time( 0.0 )
+        , _step( 0 )
+        , _bc( bc )
+        , _halo_min( 3 )
+    {
+        _mesh = std::make_shared<Mesh<MemorySpace>>(
+            global_bounding_box, global_num_cell, periodic, partitioner,
+            halo_cell_width, _halo_min, comm );
+
+        _bc.min = _mesh->minDomainGlobalNodeIndex();
+        _bc.max = _mesh->maxDomainGlobalNodeIndex();
+
+        _pm = std::make_shared<ProblemManager<MemorySpace>>(
+            ExecutionSpace(), _mesh, create_functor, particles_per_cell,vorticity);
+
+        MPI_Comm_rank( comm, &_rank );
+    }
+
+    void solve( const double t_final, const int write_freq ) override
+    {
+        // Output initial state.
+        outputParticles();
+	DriverGrid::interpVort( ExecutionSpace(), *_pm, _bc );
+
+	_step += 1;
+        _pm->communicateParticles( _halo_min );
+	outputParticles();
+    }
+
+
+    void outputParticles()
+    {
+
+    	    // Prefer HDF5 output over Silo. Only output if one is enabled.
+#ifdef Cabana_ENABLE_HDF5
+        Cabana::Experimental::HDF5ParticleOutput::HDF5Config h5_config;
+        Cabana::Experimental::HDF5ParticleOutput::writeTimeStep(
+            h5_config, "particles", _mesh->localGrid()->globalGrid().comm(),
+            _step, _time, _pm->numParticle(),
+            _pm->get( Location::Particle(), Field::Position() ),
+            _pm->get( Location::Particle(), Field::Vorticity() ),
+	    _pm->get( Location::Particle(), Field::Vortx() ));
+#else
+#ifdef Cabana_ENABLE_SILO
+        Cabana::Grid::Experimental::SiloParticleOutput::writeTimeStep(
+            "particles", _mesh->localGrid()->globalGrid(), _step, _time,
+            _pm->get( Location::Particle(), Field::Position() ),
+            _pm->get( Location::Particle(), Field::Vorticity() ));
+
+#else
+        if ( _rank == 0 )
+            std::cout << "No particle output enabled in Cabana. Add "
+                         "Cabana_REQUIRE_HDF5=ON or Cabana_REQUIRE_SILO=ON to "
+                         "the Cabana build if needed.";
+#endif
+#endif
+    }
+
+  private:
+    double _dt;
+    double _time;
+    int _step;
+    BoundaryCondition _bc;
+    int _halo_min;
+    std::shared_ptr<Mesh<MemorySpace>> _mesh;
+    std::shared_ptr<ProblemManager<MemorySpace>> _pm;
+    int _rank;
+};
+
+//---------------------------------------------------------------------------//
+// Creation method.
+template <class InitFunc>
+std::shared_ptr<SolverBase>
+createSolver( const std::string& exec_space, MPI_Comm comm,
+              const Kokkos::Array<double, 6>& global_bounding_box,
+              const std::array<int, 3>& global_num_cell,
+              const std::array<bool, 3>& periodic,
+              const Cabana::Grid::BlockPartitioner<3>& partitioner,
+              const int halo_cell_width, const InitFunc& create_functor,
+              const int particles_per_cell, const double vorticity,
+              const BoundaryCondition& bc )
+{
+    if ( 0 == exec_space.compare( "serial" ) ||
+         0 == exec_space.compare( "Serial" ) ||
+         0 == exec_space.compare( "SERIAL" ) )
+    {
+#ifdef KOKKOS_ENABLE_SERIAL
+        return std::make_shared<
+            ExaMPM::Solver<Kokkos::HostSpace, Kokkos::Serial>>(
+            comm, global_bounding_box, global_num_cell, periodic, partitioner,
+            halo_cell_width, create_functor, particles_per_cell, vorticity, bc );
+#else
+        throw std::runtime_error( "Serial Backend Not Enabled" );
+#endif
+    }
+    else if ( 0 == exec_space.compare( "openmp" ) ||
+              0 == exec_space.compare( "OpenMP" ) ||
+              0 == exec_space.compare( "OPENMP" ) )
+    {
+#ifdef KOKKOS_ENABLE_OPENMP
+        return std::make_shared<
+            ExaMPM::Solver<Kokkos::HostSpace, Kokkos::OpenMP>>(
+            comm, global_bounding_box, global_num_cell, periodic, partitioner,
+            halo_cell_width, create_functor, particles_per_cell, vorticity, bc );
+#else
+        throw std::runtime_error( "OpenMP Backend Not Enabled" );
+#endif
+    }
+    else if ( 0 == exec_space.compare( "cuda" ) ||
+              0 == exec_space.compare( "Cuda" ) ||
+              0 == exec_space.compare( "CUDA" ) )
+    {
+#ifdef KOKKOS_ENABLE_CUDA
+        return std::make_shared<
+            ExaMPM::Solver<Kokkos::CudaSpace, Kokkos::Cuda>>(
+            comm, global_bounding_box, global_num_cell, periodic, partitioner,
+            halo_cell_width, create_functor, particles_per_cell, vorticity, bc );
+#else
+        throw std::runtime_error( "CUDA Backend Not Enabled" );
+#endif
+    }
+    else if ( 0 == exec_space.compare( "hip" ) ||
+              0 == exec_space.compare( "Hip" ) ||
+              0 == exec_space.compare( "HIP" ) )
+    {
+#ifdef KOKKOS_ENABLE_HIP
+        return std::make_shared<ExaMPM::Solver<Kokkos::Experimental::HIPSpace,
+                                               Kokkos::Experimental::HIP>>(
+            comm, global_bounding_box, global_num_cell, periodic, partitioner,
+            halo_cell_width, create_functor, particles_per_cell, vorticity, bc );
+#else
+        throw std::runtime_error( "HIP Backend Not Enabled" );
+#endif
+    }
+    else
+    {
+        throw std::runtime_error( "invalid backend" );
+        return nullptr;
+    }
+}
+
+//---------------------------------------------------------------------------//
+
+} // end namespace ExaMPM
+
+#endif // end EXAMPM_SOLVER2_HPP
